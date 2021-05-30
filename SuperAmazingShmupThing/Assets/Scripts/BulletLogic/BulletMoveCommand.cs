e873e986a678a24480eb2d4cb314ec02c3b9c0ec
Windigo The Green <windigotp@gmail.com>
2021-05-30 17:41:01 +0800

    Cleaned some old commented lines

--- SuperAmazingShmupThing/Assets/Scripts/BulletLogic/BulletMoveCommand.cs
+++ SuperAmazingShmupThing/Assets/Scripts/BulletLogic/BulletMoveCommand.cs
@@ -30,28 +30,9 @@ namespace ShmupProject
                     _bulletManager.RemoveCommand(this);
                 }
 
-                //var rotation = *rotationPtr + *angularSpeedPtr++ * DeltaTime;
-                //*rotationPtr = rotation;
-                //positionPtr->x += speed * Mathf.Cos(rotation) * DeltaTime;
-                //positionPtr->y += speed * Mathf.Sin(rotation) * DeltaTime;
-
-                //var rotation = _bullet.rotation.y + _angularSpeed * deltaTime;
-                //_bullet.rotation = Quaternion.Euler(_bullet.rotation.x, rotation, _bullet.rotation.z);
-
                 //_bullet.transform.Rotate(Vector3.up, _angularSpeed * _speed * deltaTime);
                 _bullet.transform.Rotate(Vector3.up, _angularSpeed *180 /Mathf.PI * deltaTime);
                 _bullet.Translate(Vector3.forward * _speed * deltaTime, Space.Self);
-
-                //_bullet.Translate((Vector3.forward + new Vector3(Mathf.Sin(_angularSpeed), 0.0f, Mathf.Cos(_angularSpeed))) * _speed * deltaTime, Space.Self);
-                
-
-                //float posX = _bullet.position.x;
-                //float posY = _bullet.position.y;
-
-                //posX += _speed * Mathf.Cos(rotation) * deltaTime;
-                //posY += _speed * Mathf.Cos(rotation) * deltaTime;
-
-                //_bullet.position = new Vector3(posX, posY, _bullet.position.z);
             }
             else
                 _bulletManager.RemoveCommand(this);
